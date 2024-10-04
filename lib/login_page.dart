c48746520bdac5c570333dd04b121cf0086ca01a
ybangun <122917708+ybangun@users.noreply.github.com>
2024-10-04 19:11:13 +0700

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,81 +1,25 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 import 'home_page.dart';
+import 'bantuan_page.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
+void main() {
+  runApp(MyApp());
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _obscureText = true;
-
-  void _togglePasswordVisibility() {
-    setState(() {
-      _obscureText = !_obscureText;
-    });
-  }
-
-  void _login() {
-    final username = _usernameController.text;
-    final password = _passwordController.text;
-
-    if (username.isNotEmpty && password == "password") {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Login Berhasil!'),
-        backgroundColor: Colors.green,
-      ));
-
-      Future.delayed(Duration(seconds: 1), () {
-        Navigator.pushReplacementNamed(context, '/home', arguments: username);
-      });
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Login Gagal!'),
-        backgroundColor: Colors.red,
-      ));
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/upnLogo.png', height: 100),
-              SizedBox(height: 20),
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username'),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                obscureText: _obscureText,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscureText ? Icons.visibility : Icons.visibility_off,
-                    ),
-                    onPressed: _togglePasswordVisibility,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                child: Text('Login'),
-              ),
-            ],
-          ),
-        ),
+    return MaterialApp(
+      title: 'Supermarket App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: LoginPage(),
+      routes: {
+        '/home': (context) => HomePage(username: ModalRoute.of(context)!.settings.arguments as String),
+        '/bantuan': (context) => BantuanPage(),
+      },
     );
   }
 }
